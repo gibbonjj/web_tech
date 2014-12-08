ac01f3330b2f9f44fc9fa0f59ef96b1d0ca9959d
thedayman <gibbonjj@gmail.com>
2014-12-07 20:12:34 -0600

    added serving files

--- http_server.rb
+++ http_server.rb
@@ -6,17 +6,36 @@ puts "HTTP Server ready to accept requests!"
 loop do
   connection = server.accept
   puts "Opening a connection for request:"
+  message_line = connection.gets
+  path = message_line.split(' ')[1]
+  puts path
   while message_line = connection.gets
     puts message_line
     break if message_line.chomp == ""
   end
+  suffix = path.slice(/\.(.*)/)
+  content_type = case suffix
+                 when ".html" then 'text/html'
+                 when ".css" then 'text/css'
+                 when ".jpg" then 'image/jpeg'
+                 end
   puts "Sending response.."
-  connection.puts "HTTP/1.1 200 OK"
-  connection.puts "Date: #{Time.now.ctime}"
-  connection.puts "Content-Type: text/html"
-  connection.puts "Server: My Http Server"
-  connection.puts
-  connection.puts "Hi there, greetings from the server!"
+  document_path = "documents/#{path}"
+  if File.exists?(document_path)
+    connection.puts "HTTP/1.1 200 OK"
+    connection.puts "Date: #{Time.now.ctime}"
+    connection.puts "Content-Type: #{content_type}"
+    connection.puts "Server: My Http Server"
+    connection.puts
+    connection.puts File.read(document_path)
+  else
+    connection.puts "HTTP/1.1 404 Not Found"
+    connection.puts "Date: #{Time.now.ctime}"
+    connection.puts "Content-Type: text/html"
+    connection.puts "Server: My Http Server"
+    connection.puts
+    connection.puts "<html><h1>Can't find the page you requested</h1></html>"
+  end
   connection.close
   puts "Response sent and connection closed."
 end
